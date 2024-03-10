4a35693a8f644cbaad741b350bbe6d2917fe6b9d
Vasiliy VV <vase3k@gmail.com>
2024-03-10 23:34:26 +0300

    86 homework done

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,12 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percentValue = document.querySelectorAll(".skills__elem-percent"),
+    fillValue = document.querySelectorAll(".skills__elem-bar_filled");
+
+percentValue.forEach((elem, i) => {
+    const updateWidth = value => fillValue[i].style.width = value;
+    updateWidth(elem.value);
+    elem.addEventListener('change', e => updateWidth(e.target.value));
+});
